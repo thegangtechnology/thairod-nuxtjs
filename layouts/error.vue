089dedaf3e4999aba0270b5b08967fe1055c8c9b
Kongthong <m.kongthong@thegang.tech>
2021-08-04 11:19:08 +0700

    Error page

---
+++ layouts/error.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="centered">
+    <img :src="require('@/assets/images/error-page.svg')" alt="Error image">
+    <h1 v-if="error.statusCode === 404">
+      {{ pageNotFound }}
+    </h1>
+    <h1 v-else>
+      {{ otherError }}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'empty',
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  data () {
+    return {
+      pageNotFound: 'ขออภัย ไม่พบหน้าที่คุณต้องการ',
+      otherError: 'ขออภัย เกิดข้อผิดพลาด'
+    }
+  },
+  head () {
+    const title =
+      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+    return {
+      title
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 24px;
+  font-weight: bold;
+}
+.centered{
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
