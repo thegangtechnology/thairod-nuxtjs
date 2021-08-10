865311e05fd8d881fee3f68f91f790551205c883
Kongthong <m.kongthong@thegang.tech>
2021-08-10 21:17:04 +0700

    Move button print

--- pages/print/index.vue
+++ pages/print/index.vue
@@ -3,26 +3,89 @@
     <div class="page-card__container">
       <div class="page-card__header">
         <div class="page-header__title">
-          พิมพ์ใบจัดส่งสินค้า
+          {{ title }}
         </div>
-        <a-input
-          v-model="search"
-          class="page-header__search"
-          placeholder="ค้นหา"
-        >
-          <a-icon slot="prefix" type="search" />
-        </a-input>
+        <div v-if="tabKey !== 'all' && tabKey !== ''">
+          <div
+            v-if="updateOption === 'default'"
+            class="print-tab__buttons"
+          >
+            <div
+              v-if="!isUpdate"
+              slot="tabBarExtraContent"
+              class="print-tab__buttons"
+            >
+              <a-button
+                v-if="tabKey !== 'unprinted'"
+                class="assign-button__cta"
+                @click="isUpdate = true"
+              >
+                อัปเดตการพิมพ์ใบจัดส่ง
+              </a-button>
+              <a-button
+                v-if="tabKey !== 'printed'"
+                class="assign-button__cta primary"
+                @click="toCreatePrint"
+              >
+                พิมพ์ใบจัดส่งสินค้า
+              </a-button>
+            </div>
+          </div>
+        </div>
       </div>
-      <PrintOverview :search="search" />
+      <PrintOverview
+        :search="search"
+        :option="isUpdate"
+        @sendTabKey="onTabChange"
+        @sendTabUpdate="onTabUpdate"
+        @sendCancelTabUpdate="onCancelTabUpdate"
+        @selectedKeys="handleSelectedKeys"
+      />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component, Watch } from 'vue-property-decorator'
 
 @Component
 export default class Main extends Vue {
   search: string = ''
+  tabKey: string = ''
+  updateOption: string = 'default'
+  title: string = 'พิมพ์ใบจัดส่งสินค้า'
+  isUpdate:boolean= false
+  selectedRowKeys: number[] = []
+
+  // @Watch('isTabUpdate', { isUpdate: false })
+  @Watch('isUpdate', { immediate: true, deep: true })
+  onOptionChange () {
+    this.onTabUpdate(this.isUpdate)
+  }
+
+  mounted () {
+    this.updateOption = 'default'
+  }
+
+  onTabChange (key: string) {
+    console.log('tabKey', key)
+    this.tabKey = key
+  }
+
+  onTabUpdate (isUpdate:boolean) {
+    this.isUpdate = isUpdate
+  }
+
+  onCancelTabUpdate (isUpdate:boolean) {
+    this.isUpdate = isUpdate
+  }
+
+  toCreatePrint () {
+    this.$router.push('/print/create-print')
+  }
+
+  handleSelectedKeys (selectedRowKeys: number[]) {
+    this.selectedRowKeys = selectedRowKeys
+  }
 }
 </script>
