5d304a567a64b90cea3c7a190fea5236010b78ef
Piti <piti118@gmail.com>
2021-08-13 00:11:35 +0700

    fix month off by one

--- services/dashboard.service.ts
+++ services/dashboard.service.ts
@@ -17,7 +17,8 @@ export function getDashboardInfo () : Promise<DashboardResponse> {
 export function printShipment (dateStr: string) : Promise<AxiosResponse> {
   const dateObj = new Date(dateStr)
   const year = dateObj.getFullYear()
-  const month = dateObj.getMonth() + 1 < 10 ? '0' + dateObj.getMonth() : dateObj.getMonth()
+  const month_no = dateObj.getMonth() + 1
+  const month = month_no < 10 ? '0' + month_no : month_no
   const date = dateObj.getDate() < 10 ? '0' + dateObj.getDate() : dateObj.getDate()
   console.log(`${apiPath.printOfTheDay}/?date=${year}-${month}-${date}`)
   return $axios.get(`${apiPath.printOfTheDay}/?date=${year}-${month}-${date}`)
