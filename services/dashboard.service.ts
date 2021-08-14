ec824a95bbca0ce7ad4953fe62b340257f46204f
Piti <piti118@gmail.com>
2021-08-14 20:47:16 +0700

    fix build

--- services/dashboard.service.ts
+++ services/dashboard.service.ts
@@ -14,12 +14,12 @@ export function getDashboardInfo(): Promise<DashboardResponse> {
     })
 }
 
-export function printShipment(dateStr: string) {
+export function printShipment(dateStr: string, baseURL: string) {
   const dateObj = new Date(dateStr)
   const year = dateObj.getFullYear()
   const month_no = dateObj.getMonth() + 1
   const month = month_no < 10 ? '0' + month_no : month_no
   const date = dateObj.getDate() < 10 ? '0' + dateObj.getDate() : dateObj.getDate()
-  const url = `${process.env.server_url}${apiPath.printOfTheDay}/?date=${year}-${month}-${date}`
+  const url = `${baseURL}${apiPath.printOfTheDay}/?date=${year}-${month}-${date}`
   window.open(url, '_blank')
 }
