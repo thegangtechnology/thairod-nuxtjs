c24b627b6d6ce8e74f2be54ade37ee97d18c99f0
Narisa K <knarisa@thegang.local>
2021-08-20 16:12:21 +0700

    fix typescript bug & catch error

--- services/print.service.ts
+++ services/print.service.ts
@@ -2,9 +2,13 @@ import { $axios } from '~/utils/api'
 
 export function openPrintResponse (url: string) : void {
   const tab = window.open('')
+  if (tab === null) { return }
   $axios
     .get(url)
     .then((res) => {
       tab.document.write(res.request.response)
+    })
+    .catch(() => {
+      $nuxt.error({ statusCode: 404 })
     })
 }
